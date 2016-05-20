492398a063e2cd84309851942cd3d9605110dbaa
Pavel Chabinsky <freepk@mail.ru>
2016-05-20 18:34:15 +0300

    Add files

---
+++ zfs.go
@@ -0,0 +1,68 @@
+package zfs
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"os/exec"
+)
+
+func zfs(stdout io.Writer, stdin io.Reader, args ...string) error {
+	var stderr bytes.Buffer
+	cmd := exec.Command("zfs", args...)
+	if stdout != nil {
+		cmd.Stdout = stdout
+	}
+	if stdin != nil {
+		cmd.Stdin = stdin
+	}
+	cmd.Stderr = &stderr
+	if err := cmd.Run(); err != nil {
+		return errors.New(stderr.String())
+	}
+	return nil
+}
+
+func Create(name string) error {
+	return zfs(nil, nil, "create", name)
+}
+
+func Destroy(name string, recursively, force bool) error {
+	args := []string{"destroy"}
+	if recursively {
+		args = append(args, "-r")
+	}
+	if force {
+		args = append(args, "-f")
+	}
+	args = append(args, name)
+	return zfs(nil, nil, args...)
+}
+
+func Snapshot(name string) error {
+	return zfs(nil, nil, "snapshot", name)
+}
+
+func Send(name string, w io.Writer) error {
+	return zfs(w, nil, "send", name)
+}
+
+func SendDelta(name0, name1 string, intermediary bool, w io.Writer) error {
+	args := []string{"send"}
+	if intermediary {
+		args = append(args, "-I")
+	} else {
+		args = append(args, "-i")
+	}
+	args = append(args, name0, name1)
+	return zfs(w, nil, args...)
+}
+
+func Recv(name string, force bool, r io.Reader) error {
+	args := []string{"recv"}
+	if force {
+		args = append(args, "-F")
+	}
+	args = append(args, name)
+	return zfs(nil, r, args...)
+}
---
+++ zfs_test.go
@@ -0,0 +1,121 @@
+package zfs
+
+import (
+	"io"
+	"sync"
+	"testing"
+)
+
+const (
+	defaultPool = "tank"
+)
+
+func TestCreateDestroy(t *testing.T) {
+	name := defaultPool + "/createDestroyTest"
+	if err := Create(name); err != nil {
+		t.Fail()
+	}
+	if err := Destroy(name, false, false); err != nil {
+		t.Fail()
+	}
+}
+
+func TestSnapshot(t *testing.T) {
+	name := defaultPool + "/snapshotTest"
+	snap0 := "snap0"
+	snap1 := "snap1"
+	if err := Create(name); err != nil {
+		t.Fail()
+	}
+	if err := Snapshot(name + "@" + snap0); err != nil {
+		t.Fail()
+	}
+	if err := Snapshot(name + "@" + snap1); err != nil {
+		t.Fail()
+	}
+	if err := Destroy(name, true, false); err != nil {
+		t.Fail()
+	}
+}
+
+func TestSendRecv(t *testing.T) {
+	srcName := defaultPool + "/src"
+	dstName := defaultPool + "/dst"
+	snapFull := "full"
+	snap0 := "snap0"
+	snap1 := "snap1"
+
+	if err := Create(srcName); err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+	if err := Create(dstName); err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+
+	if err := Snapshot(srcName + "@" + snapFull); err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+
+	g := new(sync.WaitGroup)
+	g.Add(2)
+	r, w := io.Pipe()
+	go func() {
+		defer r.Close()
+		defer g.Done()
+		if err := Recv(dstName, true, r); err != nil {
+			t.Log(err)
+			t.Fail()
+		}
+	}()
+	go func() {
+		defer w.Close()
+		defer g.Done()
+		if err := Send((srcName + "@" + snapFull), w); err != nil {
+			t.Log(err)
+			t.Fail()
+		}
+	}()
+	g.Wait()
+
+	if err := Snapshot(srcName + "@" + snap0); err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+	if err := Snapshot(srcName + "@" + snap1); err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+	r, w = io.Pipe()
+	g.Add(2)
+	go func() {
+		defer r.Close()
+		defer g.Done()
+		if err := Recv(dstName, true, r); err != nil {
+			t.Log(err)
+			t.Fail()
+		}
+	}()
+	go func() {
+		defer w.Close()
+		defer g.Done()
+		name0 := srcName + "@" + snapFull
+		name1 := srcName + "@" + snap1
+		if err := SendDelta(name0, name1, true, w); err != nil {
+			t.Log(err)
+			t.Fail()
+		}
+	}()
+	g.Wait()
+
+	if err := Destroy(dstName, true, false); err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+	if err := Destroy(srcName, true, false); err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+}
